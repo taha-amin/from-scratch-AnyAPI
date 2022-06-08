ed2375b7f0ab576d4fad9f99122c71f8902ec33e
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-06-08 16:26:16 -0700

    in app.test.js wrote test for getting all books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,22 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const Book = require('../lib/models/Book');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/books should return a list of books', async () => {
+    const res = await request(app).get('/books');
+    const books = await Book.getAll();
+    const expected = books.map((book) => {
+      return { id: book.id, name: book.name };
+    });
+    expect(res.body).toEqual(expected);
   });
+
   afterAll(() => {
     pool.end();
   });
